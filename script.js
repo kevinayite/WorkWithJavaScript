2f25008cfe024b2870cd9930176f38da8f66ca1b
kevinayite <kevinayikson14@gmail.com>
2024-10-08 19:59:01 +0200

    Almost done

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuOpen = document.getElementById("menu-open");
+const menuClose = document.getElementById("menu-close");
+
+const sidebar = document.getElementById('.container .sidebar');
+
+menuOpen.addEventListener('click', ()=> sidebar.style.left ='0');
+menuClose.addEventListener('click', () => sidebar.style.left = '-100%');
+
+
+
