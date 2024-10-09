c44fed47584ec601d0f22bd2f65331e3020a6a94
baudoin <baudoinvicbolingo@gmail.com>
2024-10-09 13:07:40 +0200

    adding javascript functionality

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ menuOpen.addEventListener('click', ()=> sidebar.style.left ='0');
 menuClose.addEventListener('click', () => sidebar.style.left = '-100%');
 
 
+document.querySelector(".play-button").addEventListener("click", function () {
+  const audioPlayer = document.getElementById("audio-player");
+
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    this.classList.replace("bxs-right-arrow", "bxs-pause"); // Change icon to pause
+  } else {
+    audioPlayer.pause();
+    this.classList.replace("bxs-pause", "bxs-right-arrow"); // Change icon to play
+  }
+});
 
